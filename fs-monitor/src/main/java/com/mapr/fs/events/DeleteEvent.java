c2639a819600d1313b3061d6e2a9d7ca83ee038d
rkolesnik <r.kolesnik@cybervisiontech.com>
2016-07-08 18:47:04 +0300

    delete and modify implemented

--- fs-monitor/src/main/java/com/mapr/fs/events/DeleteEvent.java
+++ fs-monitor/src/main/java/com/mapr/fs/events/DeleteEvent.java
@@ -1,10 +1,13 @@
 package com.mapr.fs.events;
 
 import com.mapr.fs.messages.Delete;
+import org.apache.log4j.Logger;
 
+import java.io.File;
 import java.io.IOException;
 
 public class DeleteEvent implements Event {
+    private static final Logger log = Logger.getLogger(DeleteEvent.class);
     private Delete message;
 
     public DeleteEvent(Delete message) {
@@ -13,6 +16,14 @@ public class DeleteEvent implements Event {
 
     @Override
     public void execute(String volumePath) throws IOException {
+        String filePath = volumePath + "/" + message.name;
 
+        log.info("Executing delete event: " + filePath);
+        File file = new File(filePath);
+        if (!file.delete()) {
+            log.error("File/directory can not be deleted: " + filePath);
+            return;
+        }
+        log.info("File/directory deleted: " + filePath);
     }
 }
--- fs-monitor/src/main/java/com/mapr/fs/events/ModifyEvent.java
+++ fs-monitor/src/main/java/com/mapr/fs/events/ModifyEvent.java
@@ -1,10 +1,17 @@
 package com.mapr.fs.events;
 
 import com.mapr.fs.messages.Modify;
+import org.apache.commons.codec.binary.Base64;
+import org.apache.log4j.Logger;
 
+import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.RandomAccessFile;
+import java.nio.channels.FileChannel;
 
 public class ModifyEvent implements Event {
+    private static final Logger log = Logger.getLogger(ModifyEvent.class);
     private Modify message;
 
     public ModifyEvent(Modify message) {
@@ -13,6 +20,26 @@ public class ModifyEvent implements Event {
 
     @Override
     public void execute(String volumePath) throws IOException {
+        String filePath = volumePath + "/" + message.name;
 
+        log.info("Executing modify event: " + filePath);
+        truncate(filePath);
+
+        RandomAccessFile file = new RandomAccessFile(new File(filePath), "rw");
+        for (int i = 0; i < message.changedBlocks.size(); i++) {
+            long offset = message.changedBlocks.get(i);
+            String data = message.changedBlocksContent.get(i);
+
+            byte[] decoded = Base64.decodeBase64(data);
+            file.seek(offset);
+            file.write(decoded);
+        }
+        file.close();
+    }
+
+    private void truncate(String filePath) throws IOException {
+        FileChannel chan = new FileOutputStream(new File(filePath), true).getChannel();
+        chan.truncate(message.fileSize);
+        chan.close();
     }
 }
