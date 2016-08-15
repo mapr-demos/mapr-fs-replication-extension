227c444b34b05b3b74047ea9fdfbf4bd3e4e2ee6
dmitriyHavrilovich <dhavrilovich@cybervisiontech.com>
2016-08-15 15:37:54 +0300

    #38 small fix

--- common/src/main/java/com/mapr/fs/dao/dto/FileStatusDto.java
+++ common/src/main/java/com/mapr/fs/dao/dto/FileStatusDto.java
@@ -3,12 +3,14 @@ package com.mapr.fs.dao.dto;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
 
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 @EqualsAndHashCode(exclude = {"lastEvent","dateTime"})
 public class FileStatusDto {
 
