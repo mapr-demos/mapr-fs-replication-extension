d42887a9e4e3fc442dcca63dd6ab3456f97cfa2a
Vlad Storona <vstorona@cybervisiontech.com>
2016-09-09 15:40:05 +0300

    Update fsmon_start.sh

--- fsmon_start.sh
+++ fsmon_start.sh
@@ -1,13 +1,16 @@
 #!/usr/bin/env bash
 
 #Start Monitor
-java -agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=5555 -cp "`mapr classpath`:/home/mapr/mapr-fs-replication-extension/monitor/target/monitor-1.0-SNAPSHOT-jar-with-dependencies.jar" com.mapr.fs.application.Monitor
+sudo -u mapr java -agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=5555 -cp "`mapr classpath`:/home/vagrant/jars/monitor-1.0-SNAPSHOT-jar-with-dependencies.jar" com.mapr.fs.application.Monitor -p ~/jars/config.conf
 
 #Start Consumer
-java -agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=5005 -cp "`mapr classpath`:/home/mapr/mapr-fs-replication-extension/consumer/target/consumer-1.0-SNAPSHOT-jar-with-dependencies.jar" com.mapr.fs.application.Consumer
+sudo -u mapr java -agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=5005 -cp "`mapr classpath`:/home/vagrant/jars/consumer-1.0-SNAPSHOT-jar-with-dependencies.jar" com.mapr.fs.application.Consumer -p ~/jars/config.conf
 
 #Start Consumer UI
-java -jar mapr-fs-replication-extension/mapr-consumer-fs-ui/target/mapr-consumer-fs-ui-1.0-SNAPSHOT.jar
+sudo -u mapr java -jar ~/jars/mapr-consumer-fs-ui-1.0-SNAPSHOT.jar -p ~/jars/config.conf
 
 #Start Monitor UI
-java -jar mapr-fs-replication-extension/mapr-monitor-fs-ui/target/mapr-monitor-fs-ui-1.0-SNAPSHOT.jar
+sudo -u mapr java -jar ~/jars/mapr-monitor-fs-ui-1.0-SNAPSHOT.jar -p ~/jars/config.conf
+
+#Start S3 UI
+sudo -u mapr java -jar ~/jars/mapr-s3-replication-ui-1.0-SNAPSHOT.jar -p ~/jars/config.conf
