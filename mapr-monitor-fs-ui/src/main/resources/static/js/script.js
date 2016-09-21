7f38e9928d321c7af781f04763aa6017468c5cfb
Vlad Storona <vstorona@cybervisiontech.com>
2016-09-21 17:58:43 +0300

    Update monitor UI view. Refactor code.

--- mapr-monitor-fs-ui/src/main/resources/static/js/script.js
+++ mapr-monitor-fs-ui/src/main/resources/static/js/script.js
@@ -1,57 +1,56 @@
 var val = '';
 var API_ENDPOINT = ''
 $.ajax({
-  crossDomain: true
+    crossDomain: true
 });
 
-$(document).ready(function(){
+$(document).ready(function () {
     retrieve_data();
 });
-function changeStatus(name, monitoring, _id){
+function changeStatus(name, monitoring, _id) {
     $.post(`${API_ENDPOINT}/volumes`, {volume_name: name, path: _id, monitoring: !monitoring});
     window.location.reload();
 }
-function render_table(table_data){
+function render_table(table_data) {
     $('#table_body').empty();
     var rows = '';
-    table_data.forEach(function(file){
+    table_data.forEach(function (file) {
         rows += `<tr>
         <td>${file.name}</td>
         <td>${file._id}</td>
         <td><input type="checkbox" name="" id="${file.name}"`
         if (file.monitoring) {
-              rows +=  `checked></td></tr>`
-            } else {
-              rows +=  `></td></tr>`
-            }
+            rows += `checked></td></tr>`
+        } else {
+            rows += `></td></tr>`
+        }
     });
-    $('#table_body' ).html(rows);
-    table_data.forEach(function(file){
-            $(`#${file.name}`).click(function() {
-                changeStatus(file.name, file.monitoring, file._id);
-            });
+    $('#table_body').html(rows);
+    table_data.forEach(function (file) {
+        $(`#${file.name}`).click(function () {
+            changeStatus(file.name, file.monitoring, file._id);
         });
+    });
 }
 
 
 function retrieve_data() {
 
-    $.get(`${API_ENDPOINT}/volumes`, function( data ) {
+    $.get(`${API_ENDPOINT}/volumes`, function (data) {
         console.log(data);
         render_table(data);
 
     });
-    setTimeout(retrieve_data, 2*1000)
+    setTimeout(retrieve_data, 2 * 1000)
 }
 
 
-
-$(`#volume_form`).on('submit', function(event) {
+$(`#volume_form`).on('submit', function (event) {
     handler_click(this[0].value, this[1].value);
     console.log("Success!")
 });
 
 
 function handler_click(volume_name, path) {
-    $.post( `${API_ENDPOINT}/volumes`, { volume_name: volume_name, path: path, monitoring: true } );
+    $.post(`${API_ENDPOINT}/volumes`, {volume_name: volume_name, path: path, monitoring: true});
 }
